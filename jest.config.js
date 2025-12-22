98a91b67d147e4f8b96cfa02efc76c1597508788
xtaltia <aletabix@pm.me>
2025-12-22 21:45:11 +0000

    jest extension config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  moduleNameMapper: {
+      "(.+)\\.js": "$1", // Faz you legend <3
+  },
   transform: {
       // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
       // '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
-      '^.+\\.ts$': [
+      '^.+\\.[tj]s$': [
         'ts-jest',
         {
           diagnostics: {
