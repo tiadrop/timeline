a08d569bd298bcba3047ce3453aad250899b0bcc
Aleta <aletabix@pm.me>
2026-04-07 06:11:33 +0100

    update dev deps

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,9 @@
 module.exports = {
   testEnvironment: "node",
   moduleNameMapper: {
-      "(.+)\\.js": "$1", // Faz you legend <3
+      "(.+)\\.js": "$1",
   },
   transform: {
-      // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
-      // '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
       '^.+\\.[tj]s$': [
         'ts-jest',
         {
