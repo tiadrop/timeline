9cdf7e1f529b907d2c4ac0c9ed94643d821368d7
xtaltia <aletabix@pm.me>
2026-05-30 02:19:58 +0100

    export PointEmittter

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { ProgressionEmitter } from "./emitters.js";
 
 export { Timeline, ChainingInterface } from "./timeline.js";
 export { animate } from "./animate.js";
-export { TimelinePoint, PointEvent } from "./point.js";
-export { TimelineRange } from "./range.js";
+export { type TimelinePoint, PointEvent, type PointEmitter } from "./point.js";
+export { type TimelineRange } from "./range.js";
 export { type Emitter, type ProgressionEmitter, UnsubscribeFunc } from "./emitters.js";
 export { easers } from "./easing.js";
 
